1aa9349eb2334f4cbbd61bcb8121809d29f3d93b
Emma Segolsson <emmasegolsson@live.com>
2018-11-08 14:53:41 +0100

    Fin hälsing till er

--- diatot_dev/src/App.js
+++ diatot_dev/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Ändra <code>src/App.js</code> så ska vi göra kul.
+            Ändra <code>src/App.js</code> så ska vi göra kul. Ni är väldig smarta som kommit på att man ska göra såhär.
           </p>
           <a
             className="App-link"
