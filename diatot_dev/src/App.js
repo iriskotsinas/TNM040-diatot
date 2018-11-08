53d8854691b5e6087a4fcb1e33d33ed327f9ddff
Linn Storeund <storesundlinn@gmail.com>
2018-11-08 14:49:36 +0100

    skrivit till llll

--- diatot_dev/src/App.js
+++ diatot_dev/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Ändra <code>src/App.js</code> så ska vi göra kul.
+            Ändra <code>src/App.js</code> så ska vi göra kullll.
           </p>
           <a
             className="App-link"
