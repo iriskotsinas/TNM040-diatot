1e5f97cbc50d58fa205738da2517ced3deb15855
Emma Segolsson <emmasegolsson@live.com>
2018-11-15 00:34:34 +0100

    Fixat menyn så den anpassas efter hela sidan samt adderat bakgrundsfärg och graf

--- diatot_dev/src/App.js
+++ diatot_dev/src/App.js
@@ -2,36 +2,38 @@ import React, { Component } from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
 import logo from './logo.png';
+import bloodglucose from './bloodglucose.jpg';
 
 class App extends Component {
   render() {
     return (
     <div>
-      <div style={{height: '200px', position: 'relative', background: '#34888C'}}>
-        <Layout fixedHeader>
-          <Header transparent title="DIATOT" style={{color: 'white'}}>
-            <div class="center">
-              <img src={logo} height="100" alt="logo" />
-            </div>
-              <Navigation>
-                  <a href="/">Hej</a>
-                  <a href="/">på</a>
-                  <a href="/">dig</a>
-                  <a href="/">min bror</a>
-              </Navigation>
-          </Header>
-          <Drawer title="Title">
-              <Navigation>
-                  <a href="/">Settings</a>
-                  <a href="/">Your diary</a>
-                  <a href="/">Add</a>
-                  <a href="Sök">Blood</a>
-              </Navigation>
-          </Drawer>
-          <Content />
-      </Layout>
+        <div style={{height: '100px', position: 'fixedHeader', background: '#9bcbd6'}}>
+          <Layout fixedHeader>
+            <Header transparent style={{color: 'white'}}>
+              <div class="center">
+                <img src={logo} height="100" alt="logo" />
+              </div>
+                  <Navigation>
+                    <a href="/">Help</a>
+                  </Navigation>
+              </Header>
+            <Drawer title="Settings">
+                <Navigation>
+                    <a href="/">Settings</a>
+                    <a href="/">Your diary</a>
+                    <a href="/">Add</a>
+                    <a href="Sök">Blood</a>
+                  </Navigation>
+              </Drawer>
+            <Content style={{backgroundColor: "#9bcbd6"}}>
+              <div class="bloodgraph">
+                <img src={bloodglucose} height="400" alt="bloodglucose" />
+              </div>
+              <div className="mainButton">KNAPP</div>
+            </Content>
+          </Layout>
       </div>
-      <div className="mainButton">KNAPP</div>
     </div>
     );
   }
