a08178d02eb6fd4d0ddc6c933ee73757267f28bf
Jacob Molin <jacob.molle.molin@gmail.com>
2018-11-15 13:22:11 +0100

    Försökt importera @media-component

--- diatot_dev/src/App.js
+++ diatot_dev/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import Media from "react-media";  // using ES modules
 import logo from './logo.png';
 import bloodglucose from './bloodglucose.jpg';
+
 
 class App extends Component {
   render() {
     return (
     <div>
+    {/* using CommonJS modules*/}
+    {/*var Media = require("react-media");*/}
         <div style={{height: '100px', position: 'fixedHeader', background: '#9bcbd6'}}>
           <Layout fixedHeader>
             <Header transparent style={{color: 'white'}}>
