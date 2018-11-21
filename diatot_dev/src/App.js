ba00e4a53cbb5db91aab9c62bf815508aad9311f
Iris Kotsinas <iris.kotsinas@gmail.com>
2018-11-21 15:10:50 +0100

    Skapade components, nya sidor och klickbara knappar

--- diatot_dev/src/App.js
+++ diatot_dev/src/App.js
@@ -1,50 +1,31 @@
 import React, { Component } from 'react';
-import './App.css';
-import { Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
-import Media from "react-media";  // using ES modules
-import logo from './logo.png';
-import bloodglucose from './bloodglucose.jpg';
-
+import {Route, BrowserRouter} from 'react-router-dom';
+import FirstPage from "./components/FirstPage";
+import Log from "./components/Log";
 
 class App extends Component {
   render() {
     return (
-    <div>
-    {/* using CommonJS modules*/}
-    {/*var Media = require("react-media");*/}
-        <div style={{height: '100px', position: 'fixedHeader', background: '#9bcbd6'}}>
-          <Layout fixedHeader>
-            <Header transparent style={{color: 'white'}}>
-              <div class="center">
-                <img src={logo} height="100" alt="logo" />
-              </div>
-                  <Navigation>
-                    <a href="/">Help</a>
-                  </Navigation>
-              </Header>
-            <Drawer title="Settings">
-                <Navigation>
-                    <a href="/">Settings</a>
-                    <a href="/">Your diary</a>
-                    <a href="/">Add</a>
-                    <a href="Sök">Blood</a>
-                  </Navigation>
-              </Drawer>
-            <Content style={{backgroundColor: "#9bcbd6"}}>
-              <div class="bloodgraph">
-                <img src={bloodglucose} height="400" alt="bloodglucose" />
-              </div>
-              <div className="mainButton">KNAPP</div>
-              <div className="center">
-                <div className="smallButtons">KNAPP2</div>
-                <div className="smallButtons">KNAPP3</div>
-              </div>
-            </Content>
-          </Layout>
-      </div>
-    </div>
+      <BrowserRouter>
+        <div>
+          <Route exact={true} path='/' render={() => (
+            <div className="App">
+              <FirstPage/>
+            </div>
+          )}/>
+          <Route exact={true} path='/log' render={() => (
+            <div className="App">
+              <Log/>
+            </div>
+          )}/>
+          <Route exact={true} path='/firstpage' render={() => (
+            <div className="App">
+              <FirstPage/>
+            </div>
+          )}/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
-
 export default App;
---
+++ diatot_dev/src/components/Log.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import logo from './logo.png';
+//import FirstPage from "./components/FirstPage";
+
+export default class Log extends Component {
+  render() {
+    return (
+    <div>
+    {/* using CommonJS modules*/}
+    {/*var Media = require("react-media");*/}
+        <div style={{height: '100px', position: 'fixedHeader', background: '#9bcbd6'}}>
+          <Layout fixedHeader>
+            <Header transparent style={{color: 'white'}}>
+              <div class="center">
+                <a href="firstpage">
+                  <img src={logo} height="100" alt="logo"/>
+                </a>
+              </div>
+                  <Navigation>
+                    <a href="/">Help</a>
+                  </Navigation>
+              </Header>
+            <Drawer title="Settings">
+                <Navigation>
+                    <a href="/">Settings</a>
+                    <a href="/">Your diary</a>
+                    <a href="/">Add</a>
+                    <a href="Sök">Blood</a>
+                  </Navigation>
+              </Drawer>
+            <Content style={{backgroundColor: "#9bcbd6"}}>
+            </Content>
+          </Layout>
+      </div>
+    </div>
+    );
+  }
+}
