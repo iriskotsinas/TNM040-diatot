671ae119de38f6711fe233816b4ec15d6f406c59
Emma Segolsson <emmasegolsson@live.com>
2018-11-28 13:42:33 +0100

    Checkboxes

--- diatot_dev/src/components/Log.js
+++ diatot_dev/src/components/Log.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import { Layout, Header, Navigation, Drawer, Content, Checkbox } from 'react-mdl';
 import logo_bgtrans from './img/logo_bgtrans.png';
 //import FirstPage from "./components/FirstPage";
 
